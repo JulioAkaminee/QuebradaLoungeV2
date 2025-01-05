2b97dc133b16a59f9633d0464e23541dcfb38b63
JulioAkaminee <juliooakamine@gmail.com>
2025-01-04 22:58:43 -0300

    fazendo animação utilizando o framer motion, ao inves de fazer na mão o que demoraria muito

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,6 +1,7 @@
 import Header from '../components/header/header'
 import Head from 'next/head';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
@@ -11,10 +12,31 @@ export default function Home() {
       <Header />
     {/* Seção do banner */}
     <section className='sectionBanner'>
-  <h1 className='titleBannerJackDaniels'>
-    <span className='titleBannerStart'>Na quebrada e para a quebrada: </span>bebidas pra todos os momentos.
-  </h1>
-  <button className='btnVerPromoções'>Ver Promoções</button>
+    <motion.h1
+  className="titleBannerJackDaniels"
+  initial={{ opacity: 0, y: 20, transform: 'translate(-50%, -40%)' }} 
+  animate={{ opacity: 1, y: 0, transform: 'translate(-50%, -50%)' }}    
+  transition={{
+    duration: 1, 
+    delay: 0.5,
+    ease: [0.25, 0.8, 0.25, 1], 
+  }}
+>
+  <span className='titleBannerStart'>Na quebrada e para a quebrada: </span>bebidas pra todos os momentos.
+</motion.h1>
+
+<motion.button 
+  className='btnVerPromoções'
+  initial={{ opacity: 0, y: 20, transform: 'translate(-50%, -30%)' }} 
+  animate={{ opacity: 1, y: 0, transform: 'translate(-50%, -50%)' }}    
+  transition={{
+    duration: 1, 
+    delay: 1,  
+    ease: [0.25, 0.8, 0.25, 1], 
+  }}
+>
+  Ver Promoções
+</motion.button>
 </section>
      
     </div>
