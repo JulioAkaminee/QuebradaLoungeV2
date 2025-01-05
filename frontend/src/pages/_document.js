cd0644188d844979df40c689cbcb9336d259c865
JulioAkaminee <juliooakamine@gmail.com>
2025-01-05 00:40:24 -0300

    adicionando google analytics

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -6,18 +6,35 @@ class MyDocument extends Document {
       <Html lang="pt-br">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          {/* Corrigido a URL do Material Symbols */}
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=swap" />
           
-          {/* Pré-conectar para melhorar performance (corrigido o "crossorigin") */}
+          {/* Link para o Google Material Symbols */}
+          <link 
+            rel="stylesheet" 
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=swap" 
+          />
+
+          {/* Pré-conectar para melhorar a performance */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 
+          {/* Script do Google Analytics */}
+          <script 
+            async 
+            src="https://www.googletagmanager.com/gtag/js?id=G-1Q2WVZEC8B" 
+          />
+          <script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-1Q2WVZEC8B');
+            `}
+          </script>
         </Head>
         <body style={{background:"#17171A"}}>
           <Main />
           <NextScript />
-    
         </body>
       </Html>
     );
