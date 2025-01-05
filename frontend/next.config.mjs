0502d99c6d49609f46066fc67b11bc740bcb1232
JulioAkaminee <juliooakamine@gmail.com>
2025-01-04 21:44:59 -0300

    configurando o next para deploy

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+   
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import '../../styles/componentes/header.css';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
