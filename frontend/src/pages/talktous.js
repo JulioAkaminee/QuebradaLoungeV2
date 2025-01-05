6b88fcea01aa2f5e4fafafb016e9ff038ad88abb
JulioAkaminee <juliooakamine@gmail.com>
2025-01-04 21:41:12 -0300

    configurando o next

--- frontend/src/pages/talktous.js
+++ frontend/src/pages/talktous.js
@@ -2,19 +2,12 @@ import Header from "@/components/header/header";
 import { useState } from "react";
 
 export default function TalkToUs() {
-    const [contador,SetContador] = useState(0);
-        
-    const acrescentar = () =>{
-        SetContador(contador+1)
-    }
-     
+   
     return (
         <>
-        
         <Header/>
 
-        <button onClick={acrescentar}>acrescentar</button>
-        <p >{contador}</p>
+    
         </>
     );
 }
